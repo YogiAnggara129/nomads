a315fb9e6c183fac790783d8508c417d36dedd51
Yogi Anggara <yogianggaraaa@gmail.com>
2025-10-07 11:51:06 +0700

    feat(home): slicing popular content to footer

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -116,7 +116,222 @@
                 </div>
             </div>
         </section>
+
+        {{-- TODO: NEED CHECK (a50MQ2pmjG/100) - START --}}
+        <section class="section-popular-content" id="popularContent">
+            <div class="container">
+                <div class="section-popular-travel row justify-content-center">
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card-travel text-center d-flex flex-column"
+                            style="background-image: url('img/travel-1.jpg');">
+                            <div class="travel-country">INDONESIA</div>
+                            <div class="travel-location">DERATAN, BALI</div>
+                            <div class="travel-button mt-auto">
+                                <a href="#" class="btn btn-travel-details px-4">
+                                    View Details
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card-travel text-center d-flex flex-column"
+                            style="background-image: url('img/travel-2.jpg');">
+                            <div class="travel-country">INDONESIA</div>
+                            <div class="travel-location">BROMO, MALANG</div>
+                            <div class="travel-button mt-auto">
+                                <a href="#" class="btn btn-travel-details px-4">
+                                    View Details
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card-travel text-center d-flex flex-column"
+                            style="background-image: url('img/travel-3.jpg');">
+                            <div class="travel-country">INDONESIA</div>
+                            <div class="travel-location">NUSA PENIDA</div>
+                            <div class="travel-button mt-auto">
+                                <a href="#" class="btn btn-travel-details px-4">
+                                    View Details
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card-travel text-center d-flex flex-column"
+                            style="background-image: url('img/travel-4.jpg');">
+                            <div class="travel-country">INDONESIA</div>
+                            <div class="travel-location">DUBAI</div>
+                            <div class="travel-button mt-auto">
+                                <a href="#" class="btn btn-travel-details px-4">
+                                    View Details
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-networks" id="networks">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h2>Our Networks</h2>
+                        <p>
+                            Companies are trusted us
+                            <br />
+                            more than just a trip
+                        </p>
+                    </div>
+                    <div class="col-md-8 text-center">
+                        <img src="img/partner.png" class="img-patner" />
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-testimonials-heading" id="testimonialsHeading">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center">
+                        <h2>They Are Loving Us</h2>
+                        <p>
+                            Moments were giving them
+                            <br />
+                            the best experience
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-testimonials-content" id="testimonialsContent">
+            <div class="container">
+                <div class="section-popular-travel row justify-content-center match-height">
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="card card-testimonial text-center">
+                            <div class="testimonial-content">
+                                <img src="{{ asset('img/avatar-1.png') }}" alt=""
+                                    class="mb-4 rounded-circle" />
+                                <h3 class="mb-4">Angga Risky</h3>
+                                <p class="testimonials">
+                                    “ It was glorious and I could not stop to say wohooo for
+                                    every single moment Dankeeeeee “
+                                </p>
+                            </div>
+                            <hr />
+                            <p class="trip-to mt-2">Trip to Ubud</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="card card-testimonial text-center">
+                            <div class="testimonial-content">
+                                <img src="{{ asset('img/avatar-2.png') }}" alt=""
+                                    class="mb-4 rounded-circle" />
+                                <h3 class="mb-4">Shayna</h3>
+                                <p class="testimonials">
+                                    “ The trip was amazing and I saw something beautiful in that
+                                    Island that makes me want to learn more “
+                                </p>
+                            </div>
+                            <hr />
+                            <p class="trip-to mt-2">Trip to Nusa Penida</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="card card-testimonial text-center">
+                            <div class="testimonial-content mb-auto">
+                                <img src="{{ asset('img/avatar-3.png') }}" alt=""
+                                    class="mb-4 rounded-circle" />
+                                <h3 class="mb-4">Shabrina</h3>
+                                <p class="testimonials">
+                                    “ I loved it when the waves was shaking harder — I was
+                                    scared too “
+                                </p>
+                            </div>
+                            <hr />
+                            <p class="trip-to mt-2">Trip to Karimun Jawa</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <a href="#" class="btn btn-need-help px-4 mt-4 mx-1">
+                            I Need Help
+                        </a>
+                        <a href="#" class="btn btn-get-started px-4 mt-4 mx-1">
+                            Get Started
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {{-- TODO: NEED CHECK (a50MQ2pmjG/100) - END --}}
     </main>
+
+    {{-- TODO: NEED CHECK (a50MQ2pmjG/100) - START --}}
+    <footer class="section-footer mt-5 mb-4 border-top">
+        <div class="container pt-5 pb-5">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-12 col-lg-3">
+                                    <h5>FEATURES</h5>
+                                    <ul class="list-unstyled">
+                                        <li>
+                                            <a href="#">Reviews</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Community</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Social Media Kit</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Affiliate</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-12 col-lg-3">
+                                    <h5>ACCOUNT</h5>
+                                    <ul class="list-unstyled">
+                                        <li><a href="#">Refund</a></li>
+                                        <li><a href="#">Security</a></li>
+                                        <li><a href="#">Rewards</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-12 col-lg-3">
+                                    <h5>COMPANY</h5>
+                                    <ul class="list-unstyled">
+                                        <li><a href="#">Career</a></li>
+                                        <li><a href="#">Help Center</a></li>
+                                        <li><a href="#">Media</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-12 col-lg-3">
+                                    <h5>Get Connected</h5>
+                                    <ul class="list-unstyled">
+                                        <li>Jakarta Selatan</li>
+                                        <li>Indonesia</li>
+                                        <li>0821 - 2222 - 8888</li>
+                                        <li>support@nomads.id</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row border-top justify-content-center align-items-center pt-4">
+                <div class="col-auto text-gray-500 font-weight-light">
+                    2025 Copyright Nomads • All rights reserved • Made in Jakarta
+                </div>
+            </div>
+        </div>
+    </footer>
+    {{-- TODO: NEED CHECK (a50MQ2pmjG/100) - END --}}
 
     @include('partials.scripts')
 </body>
