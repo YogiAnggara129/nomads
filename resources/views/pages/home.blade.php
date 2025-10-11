c21f94f6e6ea20d53df308ce7c8c9b9f72431505
Yogi Anggara <yogianggaraaa@gmail.com>
2025-10-12 00:52:25 +0700

    refactor(home): use app layout

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,69 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @include('partials.styles')
-</head>
-
-<body>
-    <div class="container">
-        {{-- Navbar - start --}}
-        <nav class="row navbar navbar-expand-lg navbar-light bg-white">
-            <a href="#" class="navbar-brand">
-                <img src="{{ asset('img/logo.png') }}" alt="Logo NOMADS">
-            </a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navb">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navb">
-                <ul class="navbar-nav ml-auto mr-3">
-                    <li class="nav-item mx-md-2">
-                        <a href="#" class="nav-link active">Home</a>
-                    </li>
-                    <li class="nav-item mx-md-2">
-                        <a href="#" class="nav-link">Paket Travel</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">
-                            Services
-                        </a>
-                        <div class="dropdown-menu">
-                            <a href="#" class="dropdown-item">Link</a>
-                            <a href="#" class="dropdown-item">Link</a>
-                            <a href="#" class="dropdown-item">Link</a>
-                        </div>
-                    </li>
-                    <li class="nav-item mx-md-2">
-                        <a href="#" class="nav-link">Testimonial</a>
-                    </li>
-
-                    {{-- Mobile button - start --}}
-                    <form action="" class="form-inline d-sm-block d-md-none">
-                        <button class="btn btn-login my-2 my-sm-0">
-                            Masuk
-                        </button>
-                    </form>
-                    {{-- Mobile button - end --}}
-
-                    {{-- Desktop button - start --}}
-                    <form action="" class="form-inline my-2 my-lg-0 d-none d-md-block">
-                        <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4">
-                            Masuk
-                        </button>
-                    </form>
-                    {{-- Desktop button - end --}}
-                </ul>
-            </div>
-        </nav>
-        {{-- Navbar - end --}}
-
-    </div>
+<x-layouts.app :title="__('NOMADS - Home')">
     <header class="text-center">
         <h1>
             Explore The Beautiful World
@@ -209,8 +144,7 @@
                     <div class="col-sm-6 col-md-6 col-lg-4">
                         <div class="card card-testimonial text-center">
                             <div class="testimonial-content">
-                                <img src="{{ asset('img/avatar-1.png') }}" alt=""
-                                    class="mb-4 rounded-circle" />
+                                <img src="{{ asset('img/avatar-1.png') }}" alt="" class="mb-4 rounded-circle" />
                                 <h3 class="mb-4">Angga Risky</h3>
                                 <p class="testimonials">
                                     “ It was glorious and I could not stop to say wohooo for
@@ -224,8 +158,7 @@
                     <div class="col-sm-6 col-md-6 col-lg-4">
                         <div class="card card-testimonial text-center">
                             <div class="testimonial-content">
-                                <img src="{{ asset('img/avatar-2.png') }}" alt=""
-                                    class="mb-4 rounded-circle" />
+                                <img src="{{ asset('img/avatar-2.png') }}" alt="" class="mb-4 rounded-circle" />
                                 <h3 class="mb-4">Shayna</h3>
                                 <p class="testimonials">
                                     “ The trip was amazing and I saw something beautiful in that
@@ -239,8 +172,7 @@
                     <div class="col-sm-6 col-md-6 col-lg-4">
                         <div class="card card-testimonial text-center">
                             <div class="testimonial-content mb-auto">
-                                <img src="{{ asset('img/avatar-3.png') }}" alt=""
-                                    class="mb-4 rounded-circle" />
+                                <img src="{{ asset('img/avatar-3.png') }}" alt="" class="mb-4 rounded-circle" />
                                 <h3 class="mb-4">Shabrina</h3>
                                 <p class="testimonials">
                                     “ I loved it when the waves was shaking harder — I was
@@ -266,74 +198,4 @@
         </section>
         {{-- TODO: NEED CHECK (a50MQ2pmjG/100) - END --}}
     </main>
-
-    {{-- TODO: NEED CHECK (a50MQ2pmjG/100) - START --}}
-    <footer class="section-footer mt-5 mb-4 border-top">
-        <div class="container pt-5 pb-5">
-            <div class="row justify-content-center">
-                <div class="col-12">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="row">
-                                <div class="col-12 col-lg-3">
-                                    <h5>FEATURES</h5>
-                                    <ul class="list-unstyled">
-                                        <li>
-                                            <a href="#">Reviews</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Community</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Social Media Kit</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Affiliate</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="col-12 col-lg-3">
-                                    <h5>ACCOUNT</h5>
-                                    <ul class="list-unstyled">
-                                        <li><a href="#">Refund</a></li>
-                                        <li><a href="#">Security</a></li>
-                                        <li><a href="#">Rewards</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col-12 col-lg-3">
-                                    <h5>COMPANY</h5>
-                                    <ul class="list-unstyled">
-                                        <li><a href="#">Career</a></li>
-                                        <li><a href="#">Help Center</a></li>
-                                        <li><a href="#">Media</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col-12 col-lg-3">
-                                    <h5>Get Connected</h5>
-                                    <ul class="list-unstyled">
-                                        <li>Jakarta Selatan</li>
-                                        <li>Indonesia</li>
-                                        <li>0821 - 2222 - 8888</li>
-                                        <li>support@nomads.id</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container-fluid">
-            <div class="row border-top justify-content-center align-items-center pt-4">
-                <div class="col-auto text-gray-500 font-weight-light">
-                    2025 Copyright Nomads • All rights reserved • Made in Jakarta
-                </div>
-            </div>
-        </div>
-    </footer>
-    {{-- TODO: NEED CHECK (a50MQ2pmjG/100) - END --}}
-
-    @include('partials.scripts')
-</body>
-
-</html>
+</x-layouts.app>
